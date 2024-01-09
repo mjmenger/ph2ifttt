6f9c9de962b98fdffc40a3c83c6db23f6ad294e3
Mark J Menger <mjmenger@users.noreply.github.com>
2024-01-09 03:16:55 +0000

    external config

--- server.js
+++ server.js
@@ -1,27 +1,37 @@
-const express = require("express");
-const axios = require('axios');
-const bodyParser = require('body-parser');
-const app = express();
-const IFTTT_EVENT = process.env.IFTTT_EVENT;
-const IFTTT_WEBHOOK_KEY = process.env.IFTTT_WEBHOOK_KEY;
-const TARGETSERVICE = `https://maker.ifttt.com/trigger/${IFTTT_EVENT}/json/with/key/${IFTTT_WEBHOOK_KEY}`;
-const PORT = 8081;
+const express           = require("express");
+const axios             = require('axios');
+const bodyParser        = require('body-parser');
+const config            = require('./config.json');
+const app               = express();
+const IFTTT_EVENT       = process.env.IFTTT_EVENT || 'defaultEvent';
+const IFTTT_WEBHOOK_KEY = process.env.IFTTT_WEBHOOK_KEY || 'defaultKey';
+const TARGETSERVICE     = `https://maker.ifttt.com/trigger/${IFTTT_EVENT}/json/with/key/${IFTTT_WEBHOOK_KEY}`;
+const HOST_NAME         = process.env.HOSTNAME || 'undefinedhostname';
+const PORT              = 8081;
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/', async(req, res) =>{
-    try {
-      console.debug(`target service: ${TARGETSERVICE}`)
-      const response = await axios.post(`https://maker.ifttt.com/trigger/${IFTTT_EVENT}/json/with/key/${IFTTT_WEBHOOK_KEY}`, req.body);
-      res.status(response.status).send(response.body);
-    } catch (error) {
-      console.error('error forwarding the data');
-      res.status(500).send('failed to forward data');
-    }
+async function handleDynamicPost(req, res, forwardUrl) {
+  try {
+    console.debug(`target service: ${forwardUrl}`)
+    let payload = req.body;
+    payload.phhost = HOST_NAME;
+    const response = await axios.post(forwardUrl, payload);
+    res.status(response.status).send(response.body);
+  } catch (error) {
+    console.error('error forwarding the data');
+    res.status(500).send('failed to forward data');
   }
-);
+}
+
+
+for (const[eventpath,eventkey] of Object.entries(config)) {
+  let forwardUrl = `https://maker.ifttt.com/trigger/${eventkey}/json/with/key/${IFTTT_WEBHOOK_KEY}`;
+  console.log(`forwarding ${eventpath} requests to ${forwardUrl}`);
+  app.post(eventpath, (req, res) => handleDynamicPost(req, res, forwardUrl));
+}
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on ${HOST_NAME} port ${PORT}`);
 });
